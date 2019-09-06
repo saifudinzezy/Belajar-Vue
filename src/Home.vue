5b1d3414a637eb8cc8540191b75e4e9213e42af3
saifudin <saifudin.zezy7@gmail.com>
2019-09-06 15:25:04 +0700

    add component

---
+++ src/Home.vue
@@ -0,0 +1,24 @@
+<template>
+<!-- harus memiliki pembungkus dan hanya boleh memlikik 1 root template -->
+  <div>
+    <p>Server Status: {{ status }}</p>
+    <hr />
+    <button @click="chengeStatus">Change Status</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      status: "Critical"
+    };
+  },
+  methods: {
+    //penulisan fungsi dengan es-6
+    chengeStatus() {
+      this.status = "Normal"; //jgn lupa titik koma
+    }
+  }
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+//import component vue.js yaitu Home.vue
+import Home from './Home.vue'
+
+//add component(name_component, object)
+Vue.component('app-server-status', Home)
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
+  el: '#app', //id
+  render: h => h(App) //template
 })
